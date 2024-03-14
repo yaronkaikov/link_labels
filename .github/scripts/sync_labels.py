7d196b50665fe0afc89fbf6786b32d9bc9c967ac
Yaron Kaikov <yaronkaikov@scylladb.com>
2024-03-14 11:22:16 +0200

    wip1-test

--- .github/scripts/sync_labels.py
+++ .github/scripts/sync_labels.py
@@ -21,6 +21,7 @@ def parser():
                         help='Github repository name (e.g., scylladb/scylladb)')
     parser.add_argument('--number', type=int, required=True,
                         help='Pull request number to sync labels from linked issue')
+    parser.add_argument('--label', type=str, required=True, help='Label to add/remove from an issue or PR')
     parser.add_argument('--is_issue', action='store_true', help='Determined if label change is in Issue or not')
     parser.add_argument('--label_action', type=str, choices=['opened', 'labeled', 'unlabeled'], required=True, help='Sync labels action')
     return parser.parse_args()
@@ -64,37 +65,30 @@ def get_linked_pr(repo, issue_number, token):
         raise Exception(f"GitHub API returned {response.status_code}: {response.text}")
 
 
-def sync_labels(issue_numbers, number, repo, label_action):
+def sync_labels(issue_numbers, pr_number, repo, label_action, label, is_issue):
     print('Sync labels from Issue to linked pull request')
     github = Github(github_token)
     for issue_number in issue_numbers:
         issue_labels = [label.name for label in github.get_repo(repo).get_issue(int(issue_number)).get_labels()]
         print(issue_labels)
         if label_action == 'opened':
-            pr_labels = [label.name for label in github.get_repo(repo).get_pull(int(number)).get_labels()]
+            pr_labels = [label.name for label in github.get_repo(repo).get_pull(int(pr_number)).get_labels()]
             for label in issue_labels:
                 if label and label not in pr_labels:
-                    github.get_repo(repo).get_pull(int(number)).add_to_labels(label)
+                    github.get_repo(repo).get_pull(int(pr_number)).add_to_labels(label)
                     print(f"Found and added issue label: {label}")
         else:
-            pr_backport_labels = [label.name for label in github.get_repo(repo).get_pull(int(number)).get_labels() if label.name.startswith('backport/')]
-            issue_backport_labels = [label.name for label in github.get_repo(repo).get_issue(int(issue_number)).get_labels() if label.name.startswith('backport/')]
-            filter_pr_labels = [label for label in pr_backport_labels if label not in issue_backport_labels]
-            filter_issue_labels = [label for label in issue_backport_labels if label not in pr_backport_labels]
-
             if label_action == 'labeled':
-                for filter_pull_request_label in filter_pr_labels:
-                    github.get_repo(repo).get_issue(int(issue_number)).add_to_labels(filter_pull_request_label)
-                for filter_issue_label in filter_issue_labels:
-                    github.get_repo(repo).get_pull(int(number)).add_to_labels(filter_issue_label)
+                if is_issue:
+                    github.get_repo(repo).get_issue(int(issue_number)).add_to_labels(label)
+                else:
+                    github.get_repo(repo).get_pull(int(pr_number)).add_to_labels(label)
 
             if label_action == 'unlabeled':
-                if filter_pr_labels:
-                    for label in filter_pr_labels:
-                        github.get_repo(repo).get_issue(int(number)).remove_from_labels(label)
-                if filter_issue_labels:
-                    for label in filter_issue_labels:
-                        github.get_repo(repo).get_issue(int(issue_number)).remove_from_labels(label)
+                if is_issue:
+                    github.get_repo(repo).get_issue(int(issue_number)).remove_from_labels(label)
+                else:
+                    github.get_repo(repo).get_issue(int(pr_number)).remove_from_labels(label)
 
 
 def main():
@@ -108,7 +102,7 @@ def main():
         pr_number = args.number
         pr_body = get_pr_request_body(args.repository, pr_number)
         issue_numbers = get_linked_issues(pr_body)
-    sync_labels(issue_numbers, pr_number, args.repository, args.label_action)
+    sync_labels(issue_numbers, pr_number, args.repository, args.label_action, args.label, args.is_issue)
 
 
 if __name__ == "__main__":
