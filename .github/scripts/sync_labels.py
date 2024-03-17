2ea015d4e9ed9f835ecceaa44f79abb1037da358
Yaron Kaikov <yaronkaikov@scylladb.com>
2024-03-17 21:57:02 +0200

    wip

--- .github/scripts/sync_labels.py
+++ .github/scripts/sync_labels.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-
 import argparse
 import os
-import re
 import sys
-import requests
 from github import Github
+import re
 
 try:
     github_token = os.environ["GITHUB_TOKEN"]
@@ -15,96 +13,77 @@ except KeyError:
 
 
 def parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--repository', type=str, required=True,
-                        help='Github repository name (e.g., scylladb/scylladb)')
-    parser.add_argument('--number', type=int, required=True,
-                        help='Pull request or issue number to sync labels from')
-    parser.add_argument('--label', type=str, default=None, help='Label to add/remove from an issue or PR')
-    parser.add_argument('--is_issue', action='store_true', help='Determined if label change is in Issue or not')
-    parser.add_argument('--label_action', type=str, choices=['opened', 'labeled', 'unlabeled'], required=True, help='Sync labels action')
-    return parser.parse_args()
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--repo', type=str, required=True, help='Github repository name (e.g., scylladb/scylladb)')
+    parse.add_argument('--number', type=int, required=True, help='Pull request or issue number to sync labels from')
+    parse.add_argument('--label', type=str, default=None, help='Label to add/remove from an issue or PR')
+    parse.add_argument('--is_issue', action='store_true', help='Determined if label change is in Issue or not')
+    parse.add_argument('--action', type=str, choices=['opened', 'labeled', 'unlabeled'], required=True, help='Sync labels action')
+    return parse.parse_args()
 
 
-def get_pr_request_body(repo, number):
-    url = f"https://api.github.com/repos/{repo}/pulls/{number}"
-    headers = {
-        'Authorization': f'token {github_token}',
-        'Accept': 'application/vnd.github.v3+json'
-    }
-    response = requests.get(url, headers=headers)
-    if response.ok:
-        pr_details = response.json()
-        return pr_details['body']
-    else:
-        print("Failed to fetch PR details. Status code:", response.status_code)
+def copy_labels_from_linked_issues(repo, pr_number):
+    pr = repo.get_pull(pr_number)
+    if pr.body:
+        linked_issue_numbers = set(re.findall(r'Fixes:? (?:#|https.*?/issues/)(\d+)', pr.body))
+        for issue_number in linked_issue_numbers:
+            try:
+                issue = repo.get_issue(int(issue_number))
+                for label in issue.labels:
+                    pr.add_to_labels(label.name)
+                print(f"Labels from issue #{issue_number} copied to PR #{pr_number}")
+            except Exception as e:
+                print(f"Error processing issue #{issue_number}: {e}")
 
 
-def get_linked_issues(pr_body, repo):
+def get_linked_pr_from_issue_number(repo, number):
+    linked_prs = []
+    for pr in repo.get_pulls(state='all'):
+        if f'{number}' in pr.body:
+            linked_prs.append(pr)
+        else:
+            break
+    return linked_prs
+
+
+def get_linked_issues(repo, number):
+    pr = repo.get_pull(number)
     pattern = re.compile(r'Fixes:? (?:#|https.*?/issues/)(\d+)', re.IGNORECASE)
-    matches = re.findall(pattern, pr_body)
+    matches = re.findall(pattern, pr.body)
     if not matches:
         raise RuntimeError("No regex matches found in the body!")
-    issue_numbers = []
+    linked_issues = []
     for match in matches:
-        issue_numbers.append(match)
+        linked_issues.append(match)
         print(f"Found issue number: {match}")
-    return issue_numbers
+    return linked_issues
 
 
-def get_linked_pr(repo, issue_number):
-    for pr in repo.get_pulls(state='all'):
-        if pr.body and f"#{issue_number}" in pr.body:
-            print(f'PR contains {issue_number} in Body')
-            return pr.number
-
-    # query = f"repo:{repo} is:pr is:open linked:issue {issue_number}"
-    # url = f"https://api.github.com/search/issues?q={query}"
-    # headers = {"Authorization": f"Bearer {token}"}
-    # response = requests.get(url, headers=headers)
-    # if response.ok:
-    #     pr = response.json().get('items', [])
-    #     return pr['number']
-    # else:
-    #     raise Exception(f"GitHub API returned {response.status_code}: {response.text}")
-
-
-def sync_labels(issue_numbers, pr_number, repo, label_action, label, is_issue):
-    print('Sync labels from Issue to linked pull request')
-    for issue_number in issue_numbers:
-        if label_action == 'opened':
-            issue_labels = [label.name for label in repo.get_issue(issue_number).get_labels()]
-            pr_labels = [label.name for label in repo.get_pull(pr_number).get_labels()]
-            for label in issue_labels:
-                if label and label not in pr_labels:
-                    repo.get_pull(pr_number).add_to_labels(label)
-                    print(f"Found and added issue label: {label}")
+def sync_labels(repo, number, label, action, is_issue=False):
+    if is_issue:
+        linked_prs_or_issues = get_linked_pr_from_issue_number(repo, number)
+        target = repo.get_pull
+    else:
+        linked_prs_or_issues = get_linked_issues(repo, number)
+        target = repo.get_issue
+    for pr_or_issue_number in linked_prs_or_issues:
+        if action == 'labeled':
+            target(int(pr_or_issue_number)).add_to_labels(label)
+            print(f"Label '{label}' successfully added.")
+        elif action == 'unlabeled':
+            target(int(pr_or_issue_number)).remove_from_labels(label)
+            print(f"Label '{label}' successfully removed.")
+        elif action == 'opened':
+            copy_labels_from_linked_issues(repo, number)
         else:
-            if label_action == 'labeled':
-                if is_issue and pr_number is not None:
-                    repo.get_pull(pr_number).add_to_labels(label)
-                else:
-                    repo.get_issue(int(issue_number)).add_to_labels(label)
-            if label_action == 'unlabeled':
-                if is_issue:
-                    repo.get_pull(pr_number).remove_from_labels(label)
-                else:
-                    repo.get_issue(int(issue_number)).remove_from_labels(label)
+            print("Invalid action. Use 'labeled', 'unlabeled' or 'opened'.")
 
 
 def main():
     args = parser()
     github = Github(github_token)
-    repo = github.get_repo(args.repository)
-
-    if args.is_issue:
-        pr_number = get_linked_pr(repo, args.number)
-        issue_numbers = [args.number]
-    else:
-        pr_number = args.number
-        pr_body = get_pr_request_body(args.repository, pr_number)
-        issue_numbers = get_linked_issues(pr_body, args.repository)
-    sync_labels(issue_numbers, pr_number, repo, args.label_action, args.label, args.is_issue)
+    repo = github.get_repo(args.repo)
+    sync_labels(repo, args.number, args.label, args.action)
 
 
 if __name__ == "__main__":
